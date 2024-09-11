ca76861e72b6892606f5c04516576a43b6596fe1
Jordanizquierdo <169169895+Jordanizquierdo@users.noreply.github.com>
2024-09-11 15:20:07 -0300

    a

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def incio(request):
+    data = {"nombre":"jordan","edad":"76","mascota":"gato"}
+    return render(request,'app1/index.html',data)
